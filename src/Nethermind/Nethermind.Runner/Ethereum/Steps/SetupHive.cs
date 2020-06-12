ecb6fa65bdc1b20f1df06efe8b100de485d7b01e
SebastianDremo <sebastiandremo@gmail.com>
2020-06-13 01:01:02 +0200

    wip

--- src/Nethermind/Nethermind.Runner/Ethereum/Steps/SetupHive.cs
+++ src/Nethermind/Nethermind.Runner/Ethereum/Steps/SetupHive.cs
@@ -40,12 +40,19 @@ namespace Nethermind.Runner.Ethereum.Steps
             bool hiveEnabled = Environment.GetEnvironmentVariable("NETHERMIND_HIVE_ENABLED")?.ToLowerInvariant() == "true";
             if (hiveEnabled)
             {
-                Console.WriteLine("hiveEnabled : " + hiveEnabled); 
-                // if (_context.BlockTree == null) throw new StepDependencyException(nameof(_context.BlockTree));
-                // if (_context.Wallet == null) throw new StepDependencyException(nameof(_context.Wallet));
-                if (_context.EthereumJsonSerializer == null) throw new StepDependencyException(nameof(_context.EthereumJsonSerializer));
-                HiveRunner hiveRunner = new HiveRunner(_context.BlockTree, _context.Wallet, _context.EthereumJsonSerializer, _context.ConfigProvider, _context.LogManager.GetClassLogger());
-                await hiveRunner.Start(cancellationToken);
+                try
+                {
+                    Console.WriteLine("hiveEnabled : " + hiveEnabled);
+                    if (_context.BlockTree == null) throw new StepDependencyException(nameof(_context.BlockTree));
+                    if (_context.Wallet == null) throw new StepDependencyException(nameof(_context.Wallet));
+                    if (_context.EthereumJsonSerializer == null) throw new StepDependencyException(nameof(_context.EthereumJsonSerializer));
+                    HiveRunner hiveRunner = new HiveRunner(_context.BlockTree, _context.Wallet, _context.EthereumJsonSerializer, _context.ConfigProvider, _context.LogManager.GetClassLogger());
+                    await hiveRunner.Start(cancellationToken);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
             }
         }
     }
