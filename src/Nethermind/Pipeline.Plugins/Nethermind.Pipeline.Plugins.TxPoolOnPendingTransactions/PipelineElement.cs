e57af1c31888a4336e4fc44aec6e8ea15e0c0b56
Katarzyna Turbiarz <kasia.turbiarz@gmail.com>
2021-04-16 11:40:44 +0200

    init && tx pool on pending plugin

---
+++ src/Nethermind/Pipeline.Plugins/Nethermind.Pipeline.Plugins.TxPoolOnPendingTransactions/PipelineElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nethermind.Core;
+using Nethermind.TxPool;
+
+namespace Nethermind.Pipeline.Plugins.TxPoolOnPendingTransactions
+{
+    public class PipelineElement<TOut> : IPipelineElement<TOut> where TOut : Transaction
+    {
+        public Action<TOut> Emit { get; set; }
+
+        public PipelineElement(ITxPool txPool)
+        {
+            txPool.NewPending += OnNewPending;
+        }
+
+        private void OnNewPending(object? sender, TxEventArgs args)
+        {
+            Emit((TOut)args.Transaction);
+        }
+    }
+}
