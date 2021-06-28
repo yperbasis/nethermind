7a0e38bc91719106e6e3a3f8b563af4f5f64f096
Tomasz K. Stanczak <tkstanczak_github@demerzel.co>
2021-06-28 16:27:07 +0100

    difficulty historian tool

---
+++ src/Nethermind/Nethermind.Tools.GasHistorian/ProgramHeaders.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Nethermind.Core;
+using Nethermind.Db;
+using Nethermind.Db.Rocks;
+using Nethermind.Db.Rocks.Config;
+using Nethermind.Logging;
+using Nethermind.Serialization.Rlp;
+
+namespace Nethermind.Tools.GasHistorian
+{
+    static class ProgramHeaders
+    {
+        private static readonly ChainLevelDecoder _chainLevelDecoder = new();
+        private static readonly HeaderDecoder _decoder = new();
+
+        static void Main(string[] args)
+        {
+            string baseDir = args[0];
+            Console.WriteLine($"Scanning blocks in {baseDir}");
+
+            RocksDbSettings chainDbSettings = new("blockInfos", "blockInfos");
+            DbOnTheRocks chainDb = new(
+                baseDir,
+                chainDbSettings,
+                DbConfig.Default,
+                LimboLogs.Instance);
+
+            RocksDbSettings headerDbSettings = new("headers", "headers");
+            DbOnTheRocks headersDb = new(
+                baseDir,
+                headerDbSettings,
+                DbConfig.Default,
+                LimboLogs.Instance);
+
+            using FileStream fs = File.OpenWrite("output.csv");
+            using StreamWriter sw = new(fs);
+            sw.WriteLine($"Block Nr,Difficulty");
+            for (int i = 0; i < 10489461; i++)
+            {
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine($"Scanning block {i}");
+                }
+
+                ChainLevelInfo? chainLevelInfo = chainDb.Get(i, _chainLevelDecoder);
+
+                BlockInfo? mainChainBlock = chainLevelInfo?.MainChainBlock;
+                if (mainChainBlock is not null)
+                {
+                    BlockHeader? header = headersDb.Get(mainChainBlock.BlockHash, _decoder);
+
+                    if (header is not null)
+                    {
+                        sw.WriteLine($"{header.Number},{header.Difficulty}");
+                    }
+                }
+            }
+        }
+    }
+}
