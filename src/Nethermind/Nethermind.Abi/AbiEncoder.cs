edb25c711d365bcb8cf1ece247c8cc5122ddd17f
SebastianDremo <sebastiandremo@gmail.com>
2021-08-25 14:32:13 +0200

    wip

--- src/Nethermind/Nethermind.Abi/AbiEncoder.cs
+++ src/Nethermind/Nethermind.Abi/AbiEncoder.cs
@@ -88,7 +88,7 @@ namespace Nethermind.Abi
 
         public object[] Decode(AbiEncodingStyle encodingStyle, AbiSignature signature, byte[] data)
         {
-            throw new Exception($"Encoding style: {encodingStyle.ToString()}, data: {data.ToHexString()}");
+            throw new Exception($"Encoding style: {encodingStyle.ToString()}, data: {data.ToHexString()}, signature: {signature.ToString()}");
             bool packed = (encodingStyle & AbiEncodingStyle.Packed) == AbiEncodingStyle.Packed; 
             bool includeSig = encodingStyle == AbiEncodingStyle.IncludeSignature;
             int sigOffset = includeSig ? 4 : 0;
