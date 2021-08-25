143e92f070304a6c820e6288ba6201d372e2095d
SebastianDremo <sebastiandremo@gmail.com>
2021-08-25 13:20:11 +0200

    wip

--- src/Nethermind/Nethermind.Abi/AbiUInt.cs
+++ src/Nethermind/Nethermind.Abi/AbiUInt.cs
@@ -70,9 +70,9 @@ namespace Nethermind.Abi
                 case <= 8:
                     return ((byte) value, length);
                 case <= 16:
-                    return value >= (UInt256)ushort.MaxValue ? ((uint) value, length) : ((ushort) value, length);
+                    return value >= (UInt256)ushort.MaxValue ? (value, length) : ((ushort) value, length);
                 case <= 32:
-                    return value >= (UInt256) uint.MaxValue ? ((ulong) value, length) : ((uint) value, length);
+                    return value >= (UInt256) uint.MaxValue ? (value, length) : ((uint) value, length);
                 case <= 64:
                     return ((ulong) value, length);
                 default:
