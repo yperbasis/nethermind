26718cb80e6969ed99597f1881a9b9daad988d53
MarekM25 <marekm2504@gmail.com>
2021-09-01 09:46:00 +0200

    Timeout check again

--- src/Nethermind/Nethermind.Network/Timeouts.cs
+++ src/Nethermind/Nethermind.Network/Timeouts.cs
@@ -20,8 +20,8 @@ namespace Nethermind.Network
 {
     public static class Timeouts
     {
-        public static readonly TimeSpan InitialConnectionDelay = TimeSpan.FromSeconds(4);
-        public static readonly TimeSpan InitialConnection = TimeSpan.FromSeconds(2);
+        public static readonly TimeSpan InitialConnectionDelay = TimeSpan.FromSeconds(3);
+        public static readonly TimeSpan InitialConnection = TimeSpan.FromSeconds(4);
         public static readonly TimeSpan TcpClose = TimeSpan.FromSeconds(5);
         public static readonly TimeSpan Eth = Synchronization.Timeouts.Eth;
         public static readonly TimeSpan P2PPing = TimeSpan.FromSeconds(3);
