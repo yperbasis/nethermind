2d9529bfb205bb9b5e66ce57d6af9f48ed4876e0
MarekM25 <marekm2504@gmail.com>
2022-03-15 10:25:35 +0100

    experiments

--- src/Nethermind/Nethermind.Merge.Plugin/MergePlugin.cs
+++ src/Nethermind/Nethermind.Merge.Plugin/MergePlugin.cs
@@ -83,7 +83,7 @@ namespace Nethermind.Merge.Plugin
                    _api.RewardCalculatorSource ?? NoBlockRewards.Instance,  _poSSwitcher);
                 _api.SealValidator = new MergeSealValidator(_poSSwitcher, _api.SealValidator);
 
-                _api.GossipPolicy = new MergeGossipPolicy(_api.GossipPolicy, _poSSwitcher, _blockFinalizationManager);
+                _api.GossipPolicy = ShouldGossip.Instance;
                 
                 _api.BlockPreprocessor.AddFirst(new MergeProcessingRecoveryStep(_poSSwitcher));
             }
