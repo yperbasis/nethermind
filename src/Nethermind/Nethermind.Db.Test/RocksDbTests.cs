a3e42715b9d1af6c46038b24bde506936ce690de
Ruben Buniatyan <rubo@users.noreply.github.com>
2022-09-08 21:20:34 +0200

    Update RockDB package to v7.5

--- src/Nethermind/Nethermind.Db.Test/RocksDbTests.cs
+++ src/Nethermind/Nethermind.Db.Test/RocksDbTests.cs
@@ -25,6 +25,6 @@ public static class RocksDbTests
 
         // Patch version check is needed
         // until the package includes the binaries for aarch64
-        Assert.AreEqual("6.29.3", $"{major}.{minor}.{patch}", "Unexpected RocksDB version");
+        Assert.AreEqual("7.5.3", $"{major}.{minor}.{patch}", "Unexpected RocksDB version");
     }
 }
